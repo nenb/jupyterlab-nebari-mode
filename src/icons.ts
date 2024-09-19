68a600271dba520776a5ff02da1a4407ffc3ab84
Nick Byrne <byrnen8@tcd.ie>
2024-09-19 18:46:57 -0300

    lint

--- src/icons.ts
+++ src/icons.ts
@@ -1,6 +1,7 @@
 import { LabIcon } from '@jupyterlab/ui-components';
 import nebariLogo from '../style/icons/nebari-logo.svg';
 import deployApp from '../style/icons/deploy-app.svg';
+
 export const nebariIcon = new LabIcon({
   name: 'nebari:logo',
   svgstr: nebariLogo
